88d4dd4f7c91dab2834fcbd5e963b7e0a3b7ea5a
kmctwebdev3 <webdev3@kmct.org>
2026-08-08 11:45:37 +0530

    floating action data enters

--- frontend/app/about/components/AboutHeroSection.jsx
+++ frontend/app/about/components/AboutHeroSection.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const AboutHeroSection = () => {
   return (
-    <section className="relative w-full h-[70vh] md:h-[85vh] flex items-center bg-slate-900" data-aos="fade-up">
+    <section className="relative w-full h-[70vh] md:h-[85vh] flex items-center pt-16 md:pt-0 bg-slate-900" data-aos="fade-up">
       {/* Background Image */}
       <div className="absolute inset-0 w-full h-full">
         <Image
